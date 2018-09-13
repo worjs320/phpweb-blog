2c355b477ca70f3ecdd87e96bfde3aeb0e1ad3bd
김재건 <32512236+worjs320@users.noreply.github.com>
2018-09-13 13:17:10 +0900

    동영상페이지 댓글 업데이트

---
+++ mcommentdelete_post.php
@@ -0,0 +1,19 @@
+<?php
+    include("./lib/dbcon.php");
+    $connect = mysqli_connect("localhost","id5424089_root","111111","id5424089_phpweb");
+    $signup=signupp();
+    if(!$_SESSION[id])Error("로그인 후 이용해 주세요");
+    
+    $id=$_GET[id];
+    $no=$_GET[no];
+    $listnumber=$_GET[listnumber];
+    $title = $_POST[title];
+    $text = $_POST[text];
+    
+    $query="delete from mcomment where no='$no' and listnumber='$listnumber' and name='$_SESSION[name]' ";
+    mysqli_query($connect,$query);
+?>
+<script>
+    alert('삭제 완료');
+    location.href='./movie.php';
+</script>
