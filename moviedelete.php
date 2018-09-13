65098d3fcfcd18ecdb4603159ea96af94a9ea38b
김재건 <32512236+worjs320@users.noreply.github.com>
2018-09-13 14:04:02 +0900

    동영상 댓글 수정

--- moviedelete.php
+++ moviedelete.php
@@ -5,11 +5,15 @@
 
     $no=$_GET[no];
     $id=$_GET[id];
+    $listnumber=$_GET[listnumber];
     $title = $_POST[title];
     $text = $_POST[text];
 
     $query="delete from movie where no='$no' and id='$id' and name='$_SESSION[name]' ";
     mysqli_query($connect,$query);
+    
+    $query2="delete from mcomment where listnumber='$listnumber' ";
+    mysqli_query($connect,$query2);
 ?>
 <script>
     alert('삭제 완료');
