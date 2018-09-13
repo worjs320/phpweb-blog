fe083edb18c5ad97288704c851b7dfe29a14b429
김재건 <32512236+worjs320@users.noreply.github.com>
2018-09-13 13:15:27 +0900

    동영상페이지 댓글 업데이트

---
+++ mcomment_post.php
@@ -0,0 +1,27 @@
+<head>
+<link href="css/bootstrap.css" rel="stylesheet">
+<head>
+
+<?php
+    include("./lib/dbcon.php");
+    $connect = mysqli_connect("localhost","id5424089_root","111111","id5424089_phpweb");
+    $signup=signupp();
+    date_default_timezone_set('Asia/Seoul');
+    
+    $listnumber = $_POST[listnumber];
+    $id = $_POST[id];
+    $name = $_POST[name];
+    $mcomtext = htmlentities($_POST[mcomtext]);
+    $date = date("Y-m-d H:i", time());
+
+    if(!$_SESSION[name])Error("로그인 후 이용해 주세요");
+    if(!$mcomtext)Error("내용을 입력하세요");
+
+    $query = "insert into mcomment(listnumber,id,name,mcomtext,date)
+        values('$listnumber','$id','$name','$mcomtext','$date')";
+    mysqli_query($connect,$query);
+?>
+<script>
+    alert('댓글 작성 완료');
+    location.href='./movie.php';
+</script>
